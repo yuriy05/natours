d5cf7552f66acedd8b9167a321df04aced385bfe
yuriy05 <dilayyura@gmail.com>
2025-02-05 23:17:41 +0200

    env configuration

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
 const app = require('./app');
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
