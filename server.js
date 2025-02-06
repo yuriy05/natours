62a569056f2755ad68a1b5aa674b0f0c0093953e
yuriy05 <dilayyura@gmail.com>
2025-02-06 23:48:06 +0200

    conected to the project remote mongo database

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './.env' });
 
-const app = require('./app');
+const db = process.env.MONGO_DB_URL.replace(
+  '<db_password>',
+  process.env.MONGO_DB_PASS
+);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('Database successfully conected...'));
 
 const port = process.env.PORT || 3001;
 
