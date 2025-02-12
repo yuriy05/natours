16fffbde2bcaa83cc3cf26a735ea331394e2a4bc
yuriy05 <dilayyura@gmail.com>
2025-02-12 19:50:51 +0200

    feat: added sorting for the API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,15 +15,23 @@ exports.getAllTours = async (req, res) => {
   console.log(req.query);
   try {
     //! Here we build the query
+    //* FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|lte|gt|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //* SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // const tours = await Tour.find()
     //   .where('duration')
