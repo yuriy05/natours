9c9294c626ee4ed98c4f2ff088d8053b34c0a8c6
yuriy05 <dilayyura@gmail.com>
2025-02-16 20:07:13 +0200

    error handling | handling unhandled routes | global error handling middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -14,5 +16,20 @@ app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'failed',
+  //   message: `Can not found ${req.originalUrl} on the server!`,
+  // });
+
+  // const err = new Error(`Can not found ${req.originalUrl} on this server!`);
+  // err.status = 'failed';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can not find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -22,3 +22,5 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
+
+//test
