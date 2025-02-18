353847ae940664b0a4a6ddef051306e4439043da
yuriy05 <dilayyura@gmail.com>
2025-02-18 22:26:28 +0200

    feat: handling invalid databse ids

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,53 @@
+const AppError = require('../utils/appError');
+
+function handleCastDBError(err) {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+}
+
+function sendErrorDev(err, res) {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    stack: err.stack,
+    message: err.message,
+  });
+}
+
+function sendErrorProd(err, res) {
+  console.log(err);
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR 💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong on the server!',
+    });
+  }
+}
+
 module.exports = (err, req, res, next) => {
   // console.error(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    let error = { ...err, name: err.name };
+
+    if (error.name === 'CastError') {
+      error = handleCastDBError(error);
+    }
+
+    sendErrorProd(error, res);
+  }
 };
