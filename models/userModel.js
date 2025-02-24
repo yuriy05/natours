e2d6bd51a6b8261bb548fd1abe0ace0938f7b322
yuriy05 <dilayyura@gmail.com>
2025-02-24 22:46:22 +0200

    managing and encrypt passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,7 +25,24 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      //This only works on CREATE and SAVE !
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same !',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = new mongoose.model('user', userSchema);
